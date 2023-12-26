11b268bc4b79af7fcf8602afe1145a252deb8001
ps895r <32717581+ps895r@users.noreply.github.com>
2023-12-26 11:27:41 -0800

    Update variables.tf
    updating name again

--- Terraform-Helm-EKS-Jenkins/variables.tf
+++ Terraform-Helm-EKS-Jenkins/variables.tf
@@ -17,7 +17,7 @@ variable "jenkins_admin_password" {
 variable "cluster_name" {
   type        = string
   description = "The name of the EKS cluster."
-  default     = "dev7-cluster"
+  default     = "dev8-cluster"
 }
 
 variable "cluster_version" {
